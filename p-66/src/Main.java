0b77f698c3eb2bcfa4c585a1c42030f0f0f6825a
Bair <aiusheev@gmail.com>
2023-07-21 01:45:28 -0400

    66. Plus One

--- p-66/src/Main.java
+++ p-66/src/Main.java
@@ -3,41 +3,23 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(plusOne(new int[]{0})));
+        System.out.println(Arrays.toString(plusOne(new int[]{9, 9, 9})));
     }
 
     public static int[] plusOne(int[] digits) {
 
-        int estVume = 0;
-        if (digits[digits.length - 1] < 9) {
-            ++digits[digits.length - 1];
-            return digits;
-        } else  {
-            digits[digits.length - 1] = 0;
-            estVume++;
-        }
-
-        for (int i = digits.length - 2; i >= 0; i--) {
-
-            if (estVume > 0) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
                 digits[i]++;
-                estVume--;
+                return digits;
             }
 
-            if (digits[i] > 9) {
-                digits[i] = 0;
-                estVume++;
-            }
-
+            digits[i] = 0;
         }
 
-        if (estVume > 0) {
-            int[] newDigits = new int[digits.length + 1];
-            newDigits[0] = 1;
-            System.arraycopy(digits, 0, newDigits, 1, digits.length);
-            return newDigits;
-        }
-
-        return digits;
+        int[] newNumber = new int[digits.length + 1];
+        newNumber[0] = 1;
+        return newNumber;
     }
 }
